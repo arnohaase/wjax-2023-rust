952004a8248179afaf87935ea4fac25f891952f9
Arno Haase <arno.haase@haase-consulting.com>
2023-11-06 14:47:57 +0100

    grep

---
+++ grep/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs::OpenOptions;
+use std::io::{BufRead, BufReader};
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+/// Find strings in text files
+struct Args {
+    #[clap(short, long)]
+    /// The file to be searched
+    path: String,
+
+    sarch_string: String,
+}
+
+fn main() -> std::io::Result<()> {
+    let args: Args = Args::parse();
+    // println!("{:?}", args);
+
+    search_in_file(&args.path, &args.sarch_string)
+}
+
+fn search_in_file(path: &str, search_string: &str) -> Result<(), std::io::Error> {
+    let file = OpenOptions::new()
+        .read(true)
+        .open(path)?;
+
+    for line in BufReader::new(file).lines() {
+        let line = line?;
+        if line.contains(search_string) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
+}
